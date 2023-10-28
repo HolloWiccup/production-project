73812b195be8fb89ba432521e97428a02b6691ef
Dimitry Nasrtdinov <hollowiccup@gmail.com>
2023-10-28 15:14:09 +0300

    add redux store, add first entity

---
+++ src/app/providers/StoreProvider/index.ts
@@ -0,0 +1,9 @@
+import type { StateSchema } from './config/StateSchema';
+import { StoreProvider } from './StoreProvider';
+import { createReduxStore } from './config/store';
+
+export {
+  StoreProvider,
+  createReduxStore,
+  StateSchema,
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,21 @@
 import { render } from 'react-dom';
 import { ErrorBoundary } from 'app/providers/ErrorBoundaries';
 import { BrowserRouter } from 'react-router-dom';
+import { StoreProvider } from 'app/providers/StoreProvider';
+import { ThemeProvider } from './app/providers/ThemeProvider';
 import App from './app/App';
-import ThemeProvider from './app/providers/ThemeProvider/ui/ThemeProvider';
 import 'app/styles/index.scss';
-
 import './shared/config/i18n/i18n';
 
 render(
-  <BrowserRouter>
-    <ErrorBoundary>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    </ErrorBoundary>
-  </BrowserRouter>,
+  <StoreProvider>
+    <BrowserRouter>
+      <ErrorBoundary>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </ErrorBoundary>
+    </BrowserRouter>
+  </StoreProvider>,
   document.getElementById('root'),
 );
